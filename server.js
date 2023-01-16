0a96acd7abf9aad5ab38b1b3f0733e411d4cb416
Abhi-Bhat18 <abhishekbhat344@gmail.com>
2023-01-17 00:30:01 +0530

    cors policy removed

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');		
-const connectDB = require('./config/database')
-const cookieJwtAuth = require('./middlewares/cookieJwtAuth') //auth middleware
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/database");
+const cookieJwtAuth = require("./middlewares/cookieJwtAuth"); //auth middleware
 
-const userRoutes = require('./routes/userRoutes')
-const paymentRoutes = require('./routes/paymentRoutes');
-const dataRoutes = require('./routes/dataRoutes');
-const adminRoutes = require('./routes/adminRoutes');
+const userRoutes = require("./routes/userRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
+const dataRoutes = require("./routes/dataRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 
-const app = express()
-const PORT = process.env.PORT
+const app = express();
+const PORT = process.env.PORT;
 
-connectDB()
+connectDB();
 
 //Middlewares
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
-app.use(cors({ 
-    origin:"https://darling-parfait-b389ea.netlify.app/", 
-    credentials:true 
-}))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
-console.log('mongo ',process.env.MONGO_URI)
+console.log("mongo ", process.env.MONGO_URI);
 
 //routes
-app.use('/api/user',userRoutes);
-app.use('/api/payment',cookieJwtAuth,paymentRoutes);
-app.use('/api/data',dataRoutes);
-app.use('/api/admin',adminRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/payment", cookieJwtAuth, paymentRoutes);
+app.use("/api/data", dataRoutes);
+app.use("/api/admin", adminRoutes);
 
-app.listen(1337,()=>{
-    console.log(`server is listening at http://127.0.0.1:${PORT}`)
-})
-		
+app.listen(1337, () => {
+  console.log(`server is listening at http://127.0.0.1:${PORT}`);
+});
