3041b521eed99487426707963051978e4729cdf3
Abhi-Bhat18 <abhishekbhat344@gmail.com>
2023-01-17 08:41:17 +0530

    cors error resolved

--- server.js
+++ server.js
@@ -18,12 +18,15 @@ connectDB();
 app.use(express.static('static/build'))
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(
-  cors({
-    credentials: true,
-  })
-);
+app.use(cors());
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 //routes
 app.use("/api/user", userRoutes);
