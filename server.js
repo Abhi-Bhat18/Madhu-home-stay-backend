7a179f9890de5a570b4abd883309507267e3690a
Abhi-Bhat18 <abhishekbhat344@gmail.com>
2023-02-13 03:37:58 +0000

    sever commit

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://madhuhomestaysirsi.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
